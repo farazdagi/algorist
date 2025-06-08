bb3fb2588467648e105afb9b6497552698be7467
Victor Farazdagi <simple.square@gmail.com>
2025-06-08 21:25:24 +0300

    feat: extend io module docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,53 @@
 /// [`Scanner`](io::Scanner), and writing output using the [`wln!`](io::wln)
 /// macro.
 ///
-/// # Example
+/// # Examples
+///
+/// The `Scanner` is designed to simplify reading input in competitive
+/// programming. It allows you to read various types of data from standard input
+/// efficiently. Moreover, it supports several most common operations like
+/// reading several test cases, strings, and vectors.
+///
+/// ## Reading a single token
+///
+/// It is less common nowadays, as most contest will provide you with several
+/// test cases, but you can still read a single token using the `Scanner`, if
+/// you need to:
+///
+/// ``` bash
+/// # Input:
+/// 1 2 3 4
+/// ```
+///
+/// ``` no_run
+/// use std::io;
+///
+/// use algorist::io::{Scanner, wln};
+///
+/// let mut scan = Scanner::new(io::stdin().lock());
+///
+/// let a: i32 = scan.next(); // reads a single token and parses it as `i32`
+/// let a: i32 = scan.i(); // same as above
+///
+/// let b: usize = scan.next();
+/// let b: usize = scan.u();
+/// ```
+///
+/// ## Reading number of test cases and processing them
+///
+/// Normally, in contest programming, you will have several test cases to
+/// process. So, the input will start with a single integer `t`, which is the
+/// number of test cases, followed by `t` test inputs.
+///
+/// `Scanner` provides a convenient method `test_cases()` to read and process
+/// multiple tests more ergonomically.
+///
+/// ``` bash
+/// # Input:
+/// 1
+/// 3
+/// 1 2 3
+/// ```
 ///
 /// ``` no_run
 /// use std::io::{self, Write};
